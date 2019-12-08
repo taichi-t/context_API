1396a220e8e3a8c7aacb33060459bbdbd9018941
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2019-12-08 13:43:49 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import CounterContext from "./contexts/counter";
+import Counter from "./components/counter";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+
+    this.state = {
+      count: 0,
+      increment: this.increment,
+      decrement: this.decrement
+    };
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    return (
+      <CounterContext.Provider value={this.state}>
+        <Counter />
+      </CounterContext.Provider>
+    );
+  }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
